4e279b9c95dd5af5b5d78b587f55f173e0806a56
Aleksandra <azakrecka@student.agh.edu.pl>
2023-12-27 02:05:10 +0100

    Czytanie bazy danych

--- Warehouse.cpp
+++ Warehouse.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "Warehouse.h"
+
+std::vector <Parts> Warehouse::Wczytaj()
+{
+	std::vector <Parts> stanMagazynu;
+
+	std::ifstream plik("Magazyn");
+
+	unsigned int ID;
+	std::string name;
+	unsigned int amount;
+
+	std::string linia;
+
+	while (std::getline(plik, linia))
+	{
+		Parts part(ID, name, amount);
+
+		stanMagazynu.push_back(part);
+
+	}
+
+	return stanMagazynu;
+}
+
+Parts::Parts(unsigned int ID, std::string name, unsigned int amount) : ID(ID), name(name), amount(amount) {}
--- Warehouse.h
+++ Warehouse.h
@@ -5,10 +5,28 @@
 #ifndef CARSERVICE_WAREHOUSE_H
 #define CARSERVICE_WAREHOUSE_H
 
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <fstream>
+#include <string>
+
 
 class Warehouse {
 public:
-    // funkcje związane z magazynem
+
+    std::vector <Parts> Wczytaj();
+};
+
+class Parts
+{
+    unsigned int ID;
+    std::string name;
+    unsigned int amount;
+
+public:
+
+    Parts(unsigned int, std::string, unsigned int);
 };
 
 

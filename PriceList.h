adad7d487743bce84ba96d2a8427d8548e1371c6
oliwiaaa333 <wiatrowskaoliwiaxxx@gmail.com>
2023-12-29 13:28:55 +0100

    Klasa Usluga - pola i zadeklarowane metody.

--- PriceList.h
+++ PriceList.h
@@ -5,6 +5,21 @@
 #ifndef CARSERVICE_PRICELIST_H
 #define CARSERVICE_PRICELIST_H
 
+#include <iostream>
+#include <vector>
+#include <string>
+
+class PriceList;
+
+class Service {
+    std::string name;
+    unsigned int price;
+    unsigned int ID;
+    public:
+        Service(std::string, unsigned int, unsigned int);
+        void Wyswietl();
+        void ZmienCene(PriceList& cennik, unsigned int nowaCena);
+};
 
 class PriceList {
 public:

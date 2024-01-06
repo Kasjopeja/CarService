f7d2198855a32c83475c879b4f9a05d3134eb337
oliwiaaa333 <wiatrowskaoliwiaxxx@gmail.com>
2024-01-06 16:28:32 +0100

    Implementacja menu do zarządzania cennikiem

--- OfficeWorker.cpp
+++ OfficeWorker.cpp
@@ -44,8 +44,45 @@ void OfficeWorker::manageWarehouse() {
 }
 
 void OfficeWorker::managePriceList() {
-    std::cout << "Zarządzanie cennikiem\n";
-    // implementacja
+    
+    PriceList priceList;
+    Service service1(1, "Wymiana opon", 100);
+    Service service2(2, "Wymiana oleju", 200);
+    Service service3(3, "Wymiana klockow", 300);
+    Service service4(4, "Wymiana tarcz hamulcowych", 400);
+
+    priceList.addService(service1);
+    priceList.addService(service2);
+    priceList.addService(service3);
+    priceList.addService(service4);
+
+    int choice;
+    do {
+        std::cout << "\nZarzadzenie cennikiem. Wybierz co chcesz zrobic:" << std::endl;
+        std::cout << "1. Wyswietl cennik." << std::endl;
+        std::cout << "2. Dodaj usluge." << std::endl;
+        std::cout << "3. Usun usluge." << std::endl;
+        std::cout << "4. Zmiana ceny uslugi." << std::endl;
+        std::cout << "0. Wyjscie." << std::endl;
+        std::cout<< "Wybor: ";
+        std::cin >> choice;
+        system("cls");
+        switch (choice) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 0:
+			break;
+		default:
+			std::cout << "Nie ma takiej opcji w menu." << std::endl;
+			break;
+		}
+    } while (choice != 0);
 }
 
 void OfficeWorker::issueInvoice() {

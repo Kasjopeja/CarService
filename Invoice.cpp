ae421782b56665d19b836250f60e8c6dfa439f67
LucjaWuls <lucjawuls@student.agh.edu.pl>
2024-01-06 01:11:02 +0100

    .h do faktur i jedna z funkcji

--- Invoice.cpp
+++ Invoice.cpp
@@ -1,1 +1,21 @@
 #include "Invoice.h"
+#include <string>
+
+void Invoice::addClient() {
+	std::string name;
+	std::string lastname;
+	int phoneNumber;
+	std::string mailAdress;
+
+	std::cout << "Imie klienta: ";
+	std::cin >> name;
+	std::cout <<" " << "Nazwisko kleinta: ";
+	std::cin >> lastname;
+	std::cout<< std::endl << "Numer telefonu klient: ";
+	std::cin >> phoneNumber;
+	std::cout << std::endl << "Adres mail klienta: ";
+	std::cin >> mailAdress;
+
+	//Jutro rano poprawie, zeby to wszytko bylo zapisywane do pliku, teraz ide spac xoxo
+
+}

03fd1b8fe63201d86c964d45ae9f235e35bb80d1
LucjaWuls <lucjawuls@student.agh.edu.pl>
2024-01-07 18:21:28 +0100

    Funkcja dodająca części cz.1

--- Invoice.cpp
+++ Invoice.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "Invoice.h"
+#include "Event.h"
 
 struct Client {
     std::string name;
@@ -9,12 +11,44 @@ struct Client {
     std::string phoneNumber;
 };
 
+
 void addClient(const Client& client, std::ofstream& invoiceFile) {
     invoiceFile << "Imiê: " << client.name << std::endl;
     invoiceFile << "Nazwisko: " << client.lastname << std::endl;
     invoiceFile << "Adres mailowy: " << client.mailAdress << std::endl;
     invoiceFile << "Numer telefonu: " << client.phoneNumber << std::endl;
     invoiceFile << "-------------------------" << std::endl;
+}
+
+void Invoice::addPartsData(const Parts& parts) {
+    std::ofstream invoiceFile("invoice.txt", std::ios::app);
+
+    if (invoiceFile.is_open()) {
+        invoiceFile << "ID: " << parts.ID << std::endl;
+        invoiceFile << "Name: " << parts.name << std::endl;
+        invoiceFile << "Amount: " << parts.amount << std::endl;
+        invoiceFile << "-------------------------" << std::endl;
+    }
+}
+
+void Print(const std::string& fileName){
+    // otworzenie pliku do odczytu
+    std::ifstream plik(fileName);
+
+    if (plik.is_open()) {
+        std::string linia;
+
+        // odczytannie i wypisanie zawartoœæ pliku linia po linii
+        while (std::getline(plik, linia)) {
+            std::cout << linia << std::endl;
+        }
+
+        // Zamknie plik
+        plik.close();
+    }
+    else {
+        std::cerr << "B³¹d: Nie uda³o siê otworzyæ pliku do odczytu." << std::endl;
+    }
 }
 
 int main() {
@@ -49,6 +83,13 @@ int main() {
         std::cerr << "B³¹d: Nie uda³o siê otworzyæ pliku do zapisu." << std::endl;
         return 1;
     }
+    // Podanie nazwê pliku, który chcemy odczytaæ
+    std::string fileName = "faktura.txt";
+
+    // Wywo³anie funkcji
+    Print(fileName);
+
+
 
     return 0;
 }
--- Invoice.h
+++ Invoice.h
@@ -5,18 +5,20 @@
 #include "Event.h"
 #include "Warehouse.h"
 #include "Invoice.cpp"
+#include <vector>
 
 class Invoice {
     
     float charge; //należność jako float
+    std::vector<Event> events;  // zlecenie w vectorze
 
 public:
-
+    std::vector<Event> getEvents() const;   // getter do wyciągania zleceń
 
     void addEvent(const Event& event);  //dodawanie zlecenie do faktury
     void addParts(Parts& parts);    //dodawanie czesci do faktury
     void addClient(const Client& client, std::ofstream& invoiceFile);   //dodaje dane klienta
-    void Print();   //drukowanie faktury
+    void Print(const std::string& fileName);   //drukowanie faktury
 };
 
 
